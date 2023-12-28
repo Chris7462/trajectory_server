316f5f95845ab5dfc703171df69e09386744caaa
chris7462 <chris7462@gmail.com>
2023-12-28 14:31:30 -0500

    Fix coding style

--- launch/trajectory_server_launch.py
+++ launch/trajectory_server_launch.py
@@ -3,16 +3,16 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    trajectory_server_node=Node(
-        package="trajectory_server",
-        executable="trajectory_server_node",
-        name="trajectory_server_node",
-        namespace = "",
+    trajectory_server_node = Node(
+        package='trajectory_server',
+        executable='trajectory_server_node',
+        name='trajectory_server_node',
+        namespace='',
         parameters=[{
-            "target_frame_name": "map",
-            "source_frame_name": "base_link",
-            "trajectory_update_rate": 10.0,
-            "trajectory_publish_rate": 10.0
+            'target_frame_name': 'map',
+            'source_frame_name': 'base_link',
+            'trajectory_update_rate': 10.0,
+            'trajectory_publish_rate': 10.0
         }]
     )
 
